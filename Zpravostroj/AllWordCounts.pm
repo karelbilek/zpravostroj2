04d02e4521db53bf95a348b2305df2be704f0e46
Karel Bilek <bilek@ufallab2.ms.mff.cuni.cz>
2011-01-29 10:31:50 +0100

    komentare

--- Zpravostroj/AllWordCounts.pm
+++ Zpravostroj/AllWordCounts.pm
@@ -1,4 +1,18 @@
 package Zpravostroj::AllWordCounts;
+#Package ke sledování celkových "reverzních" počtů
+
+#Co je tím myšleno. Pro každé slovo si držím počet článků, ve kterých je uvedeno.
+#To mám v jednom velkém souboru v data/all_count/counts.bz2
+
+#V tom souboru je hash slovo->počet
+
+#Počítám s tím, že slova se už nebudou u článků měnit. Tedy, jak už článek jednou má counts, tak není potřeba nic řešit.
+
+#Proto si pamatuji "poslední spočítaný den" a "poslední spočítaný článek v tom dni" - když pak, později,
+#potřebuji nejnovější counts, starší články už nechávám být, jenom nahraju ten hash, přičtu nové články (=novější, než to datum)
+#a do counts popřičítám slova pouze z nich
+
+
 
 use 5.008;
 use strict;
@@ -17,12 +31,16 @@ use Scalar::Util qw(reftype);
 
 
 mkdir "data";
-mkdir "data/all";
+mkdir "data/all_count";
+
+sub null_all {
+	system("rm -r data/all_count/");
+}
 
 sub get_count {
 	
 	
-	my $c = undump_bz2("data/all/counts.bz2");
+	my $c = undump_bz2("data/all_count/counts.bz2");
 	
 	if (!$c or reftype($c) ne "HASH") {
 		return ();
@@ -32,16 +50,16 @@ sub get_count {
 }
 
 sub get_last_saved {
-	if (!-e "data/all/date_last_counted") {
+	if (!-e "data/all_count/date_last_counted") {
 		return (new Date(day=>0, month=>0, year=>0), 0);
 	}
-	return (Date::get_from_file("data/all/date_last_counted"), read_file("data/all/article_last_counted"));
+	return (Date::get_from_file("data/all_count/date_last_counted"), read_file("data/all_count/article_last_counted"));
 }
 
 sub set_last_saved {
 	my ($date, $num) = @_;
-	$date->get_to_file("data/all/date_last_counted");
-	write_file("data/all/article_last_counted", $num);
+	$date->get_to_file("data/all_count/date_last_counted");
+	write_file("data/all_count/article_last_counted", $num);
 }
 
 sub set_latest_count {
@@ -84,7 +102,7 @@ sub set_latest_count {
 		return ();
 		
 	},1);
-	dump_bz2("data/all/counts.bz2", \%counts);
+	dump_bz2("data/all_count/counts.bz2", \%counts);
 	set_last_saved($last_date_now, $last_date_now->article_count-1);
 }
 
