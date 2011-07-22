e6824e2c1f7e367e8cbeb4f5573b24debb123fac
Karel B <running@bilek.vs.fortion.net>
2011-07-22 09:55:12 +0200

    hotova bakalarka

---
+++ thesis/do.pl
@@ -0,0 +1,22 @@
+$w = "prace";
+
+`rm $w.pdf`;
+
+for my $f (<$w*>) {
+	if ($f ne "$w.tex" and $f ne "$w.pdf" and $f ne "$w.bib") {
+		system("rm $f");
+	}
+}
+
+system("pdflatex $w");
+
+system("bibtex $w");
+system("pdflatex $w ");
+system("pdflatex $w");
+
+
+for my $f (<$w*>) {
+	if ($f ne "$w.tex" and $f ne "$w.pdf" and $f ne "$w.bib") {
+		system("rm $f");
+	}
+}
